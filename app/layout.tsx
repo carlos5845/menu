83a73fb84ade4468a84fdbc4d3c70b0891c02bce
carlos5845 <martinescarlos154@gmail.com>
2026-05-21 10:56:20 -0500

    v3.1

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,9 +25,9 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${anybody.variable} ${plusJakartaSans.variable} h-full antialiased`}
+      className={`${anybody.variable} ${plusJakartaSans.variable} h-full antialiased overflow-x-hidden`}
     >
-      <body className="min-h-full flex flex-col bg-[#050505] text-[#f5f5f5]">
+      <body className="min-h-full flex flex-col bg-[#050505] text-[#f5f5f5] overflow-x-hidden relative w-full">
         <main className="flex-grow">
           {children}
         </main>
